9000ef79f6f63df2670e3c575bd42df0e6618452
marcs-sus <lilianemarcos2018@gmail.com>
2026-01-26 16:49:57 -0300

    Add world border to restrict player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,10 +4,12 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export] private float Speed = 200f;
+	[Export] private float TileSize = 32.0f;
+	[Export] public Area2D WorldBorder;
 
 	private AnimatedSprite2D animatedSprite => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	private Rect2 WorldBorderRect => WorldBorder.GetNode<CollisionShape2D>("CollisionShape2D").GetShape().GetRect();
 
-	private const float TileSize = 32.0f;
 	private Vector2 targetPosition;
 	private bool isMoving = false;
 
@@ -20,8 +22,10 @@ public partial class Player : CharacterBody2D
 	{
 		if (isMoving)
 		{
+			// Move towards target position
 			Position = Position.MoveToward(targetPosition, Speed * (float)delta);
 
+			// Check if reached target position
 			if (Position == targetPosition)
 				isMoving = false;
 		}
@@ -31,6 +35,7 @@ public partial class Player : CharacterBody2D
 	{
 		if (isMoving) return;
 
+		// Handle movement input
 		if (@event.IsActionPressed("up"))
 			Move(Vector2.Up);
 		else if (@event.IsActionPressed("down"))
@@ -43,8 +48,17 @@ public partial class Player : CharacterBody2D
 
 	private void Move(Vector2 direction)
 	{
+		// Calculate new target position and rotation
 		targetPosition = Position + direction * TileSize;
+
+		// Do not move outside world border
+		if (WorldBorderRect.HasPoint(targetPosition) == false)
+			return;
+
+		Rotation = direction.Angle() + Mathf.Pi / 2;
 		//GD.Print("New Target: " + targetPosition);
+
+		// Play hop animation
 		animatedSprite.Play("hop");
 
 		isMoving = true;
