160d561d41be3809a2dc6930f7e0a0e9bef431a1
marcs-sus <lilianemarcos2018@gmail.com>
2026-01-27 17:52:59 -0300

    Add centralized global variables

---
+++ scripts/Globals.cs
@@ -0,0 +1,4 @@
+public class Globals
+{
+    public const float TILE_SIZE = 32.0f;
+}
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,7 +4,6 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export] private float Speed = 200f;
-	[Export] private float TileSize = 32.0f;
 	[Export] public Area2D WorldBorder;
 
 	private AnimatedSprite2D animatedSprite => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
@@ -13,11 +12,13 @@ public partial class Player : CharacterBody2D
 	private Vector2 targetPosition;
 	private bool isMoving = false;
 
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		targetPosition = Position;
 	}
 
+	// Called every physics frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		if (isMoving)
@@ -49,7 +50,7 @@ public partial class Player : CharacterBody2D
 	private void Move(Vector2 direction)
 	{
 		// Calculate new target position and rotation
-		targetPosition = Position + direction * TileSize;
+		targetPosition = Position + direction * Globals.TILE_SIZE;
 
 		// Do not move outside world border
 		if (WorldBorderRect.HasPoint(targetPosition) == false)
