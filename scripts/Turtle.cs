178470c499599f86be80c4374612fe2e8e1297f1
marcs-sus <lilianemarcos2018@gmail.com>
2026-02-16 22:11:17 -0300

    Implement turtle submersion behavior with collision handling

--- scripts/Turtle.cs
+++ scripts/Turtle.cs
@@ -3,16 +3,58 @@ using System;
 
 public partial class Turtle : Obstacle
 {
+	[Export] public float SubmergeDuration = 1.0f;
+	[Export] public float SubmergeInterval = 2.0f;
+
+	private CollisionShape2D collisionShape => GetNode<CollisionShape2D>("CollisionShape2D");
+	private AnimatedSprite2D animatedSprite => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+	private float submergeTimer;
+	private bool isSubmerged = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
+
+		submergeTimer = SubmergeInterval;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
+
+		// Handle submerging behavior
+		submergeTimer -= (float)delta;
+		if (submergeTimer <= 0 && isSubmerged == false)
+		{
+			Submerge();
+			submergeTimer = SubmergeDuration;
+		}
+		else if (submergeTimer <= 0 && isSubmerged == true)
+		{
+			Emerge();
+			submergeTimer = SubmergeInterval;
+		}
+	}
+
+	// Submerge the turtle, disabling its collision
+	private void Submerge()
+	{
+		isSubmerged = true;
+		collisionShape.Disabled = true;
+
+		animatedSprite.Play("submerge");
+	}
+
+	// Emerge the turtle, enabling its collision
+	private void Emerge()
+	{
+		isSubmerged = false;
+		collisionShape.Disabled = false;
+
+		animatedSprite.Play("default");
 	}
 
 	// Check if player is on turtle
